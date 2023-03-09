9c0b06a568089422b77b888d49a3a29bb8e1e367
Sylvain KAUFFEISEN <skauffeisen@campus-eni.fr>
2023-03-09 10:28:58 +0100

    ajout dans l'index vers la correction

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
             <li><a href="demo/base.php">Lundi : base.php</a></li>
             <li><a href="demo/index.php">Mardi : index.php</a></li>
             <li><a href="demo/exoFonctionsUtiles.php">Mercredi : exoFonctionsUtiles.php</a></li>
+            <li><a href="demo/classes_exo5/exo5Main.php">Jeudi : TP1 exo 5 des classes</a></li>
         </ol>
         <h2>Les bases du langage</h2>
         <ol>
